bf02cb9c433674530bef6a16362e170c860be39c
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-09-12 00:51:40 +0530

    Create transform2.js

---
+++ gcp/transform2.js
@@ -0,0 +1,35 @@
+function transform(input, emitter, context) {
+ 
+    var part = input.A;
+    var produce = input.B;
+    var procure = input.C;
+    var planner = input.D;
+    var buyer = input.E;
+    var nettable = input.F;
+
+   
+    var weeks = [
+        { date: context.getHeader('G'), quantity: input.G }, 
+        { date: context.getHeader('H'), quantity: input.H },  
+        { date: context.getHeader('I'), quantity: input.I },  
+        { date: context.getHeader('J'), quantity: input.J }   
+    ];
+
+     
+    weeks.forEach(function(week) {
+        if (week.quantity && week.quantity !== '') {   
+            emitter.emit({
+                file: input.file,               
+                sheet: input.sheet,              
+                A: part,                        
+                B: produce,                    
+                C: procure,                    
+                D: planner,                     
+                E: buyer,                     
+                F: nettable,                   
+                G: week.date,                   
+                H: week.quantity                 
+            });
+        }
+    });
+}
